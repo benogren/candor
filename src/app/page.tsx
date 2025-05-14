67515c52477e5ed28c1da8745d8a439264d5b689
Ben Ogren <ben@benogren.com>
2025-05-14 09:45:33 -0600

    home page linkedin tracking code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -188,6 +188,34 @@ export default function Home() {
           alt=""
         />
       </noscript>
+
+      {/* LinkedIn Insight Tag */}
+      <Script id="linkedin-insight" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "7235884";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          
+          (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);
+          })(window.lintrk);
+        `}
+      </Script>
+      
+      {/* LinkedIn noscript fallback */}
+      <div dangerouslySetInnerHTML={{ 
+        __html: `
+          <noscript>
+            <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=7235884&fmt=gif" />
+          </noscript>
+        `
+      }} />
       
       <Header />
 
